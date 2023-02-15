d6f444ab5a0b86bbf8d93b9ba48807c253e56563
TreySchneider95 <tschneid95@gmail.com>
2023-02-15 12:37:06 -0800

    created router to toDo page. rendering simple test page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongooseConnect();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var toDoRouter = require('./routes/toDo');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', toDoRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
